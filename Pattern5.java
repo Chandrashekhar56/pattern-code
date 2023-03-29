e52aa0d1ba454e5e932ed4cdbe2285031d3bb713
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-03-29 16:31:05 +0530

    Create Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,22 @@
+public class Pattern5
+{  
+ static void main(String[] args)  
+{  
+ int row=7,a=1;      
+  for(int i=row; i>=0; i--)  
+ {  
+
+  for (int j=0; j<=i; j++)  
+  {  
+    if((i==row)||(j==0))
+     System.out.print(a+"");
+    else if(j==i)
+     System.out.print(a+"");
+    else 
+     System.out.print(" ");
+  } 
+  a=a+1;
+  System.out.println();  
+ }  
+}  
+}  
