fcb6390f39b8cc66e03eea1a30b9da83b547b18f
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-03-29 16:05:57 +0530

    Create Pattern3.java

---
+++ Pattern3.java
@@ -0,0 +1,17 @@
+public class Main
+{  
+public static void main(String[] args)  
+{  
+ int row=4,a=10;      
+  for(int i=row; i>=0; i--)  
+ {  
+
+  for (int j=0; j<i; j++)  
+  {  
+    System.out.print(a+" "); 
+    a--;
+  }  
+  System.out.println();  
+ }  
+}  
+}  
